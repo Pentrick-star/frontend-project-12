50fa7b7ae37c917ec7125a04a6e0c2c53a87c259
Pentrick <bibenina09@mail.ru>
2025-08-22 21:17:40 +0300

    fix: добавлено отображение имени пользователя в сообщениях

--- frontend/src/components/MessageForm.jsx
+++ frontend/src/components/MessageForm.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { sendMessage } from '../store/messagesSlice';
 import { filterProfanity } from '../utils/profanityFilter';
+import { useAuth } from '../hooks/useAuth';
 
 const MessageForm = () => {
   const { t } = useTranslation();
@@ -10,6 +11,7 @@ const MessageForm = () => {
   const dispatch = useDispatch();
   const { currentChannelId } = useSelector((state) => state.channels);
   const { loading } = useSelector((state) => state.messages);
+  const { user } = useAuth();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -20,6 +22,7 @@ const MessageForm = () => {
       await dispatch(sendMessage({
         body: filteredMessage,
         channelId: currentChannelId,
+        username: user?.username || 'Unknown',
       })).unwrap();
       setMessage('');
     } catch (error) {
