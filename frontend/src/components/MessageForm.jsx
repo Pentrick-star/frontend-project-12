0a1862019501f15e015e85c3f57fd7f54f1025ca
Pentrick <bibenina09@mail.ru>
2025-09-05 23:55:09 +0300

    отображение admin

--- frontend/src/components/MessageForm.jsx
+++ frontend/src/components/MessageForm.jsx
@@ -19,7 +19,7 @@ const MessageForm = () => {
 
     try {
       const filteredMessage = filterProfanity(message.trim());
-      const username = user?.username || user?.name || user?.login || 'User';
+      const username = user?.login || user?.username || user?.name || 'User';
       
       const messageData = {
         body: filteredMessage,
