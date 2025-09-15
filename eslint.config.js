653bcc785df60fd6dd5a7b3398b26f5efd697a1b
Pentrick <bibenina09@mail.ru>
2025-09-15 20:27:58 +0300

    настройки линтера

--- eslint.config.js
+++ eslint.config.js
@@ -9,21 +9,22 @@ export default defineConfig([
   },
   {
     files: ['**/*.{js,mjs,cjs,jsx}'],
-    ignores: ['**/dist/**', '**/build/**', '**/node_modules/**'],
     plugins: { js },
     extends: ['js/recommended'],
     languageOptions: {
       globals: globals.browser,
+      sourceType: 'module',
+      ecmaVersion: 'latest',
+      parserOptions: { ecmaFeatures: { jsx: true } },
     },
   },
   {
     files: ['**/*.{js,mjs,cjs,jsx}'],
-    ignores: ['**/dist/**', '**/build/**', '**/node_modules/**'],
     ...pluginReact.configs.flat.recommended,
-    settings: {
-      react: {
-        version: 'detect',
-      },
+    settings: { react: { version: 'detect' } },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-vars': 'error',
     },
   },
 ])
