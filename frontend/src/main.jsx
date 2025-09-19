f4e2362b37f3ecc140a8c07519bce1a3e6bb5fe6
pentrick <bibenina09@mail.ru>
2025-09-19 14:43:48 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,16 +3,11 @@ import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.jsx'
-import './index.css'
 import './i18n'
 import './utils/rollbar'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <>
-      <App />
-    </>
+    <App />
   </StrictMode>,
 )
