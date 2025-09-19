1c7d7e78cf23763413b841ad0e46fa0b54af3e7b
Pentrick <bibenina09@mail.ru>
2025-09-19 10:24:29 +0300

    fix(build): remove @rollbar/react import; use plain Rollbar SDK

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,20 +5,15 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.jsx'
 import './index.css'
 import './i18n'
-import { Provider, ErrorBoundary } from '@rollbar/react'
-import { rollbarConfig } from './utils/rollbar'
+import './utils/rollbar'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider config={rollbarConfig}>
-      <ErrorBoundary fallbackUI={<div>Something went wrong</div>}>
-        <>
-          <App />
-          <ToastContainer position="top-right" autoClose={3000} />
-        </>
-      </ErrorBoundary>
-    </Provider>
+    <>
+      <App />
+      <ToastContainer position="top-right" autoClose={3000} />
+    </>
   </StrictMode>,
 )
