43a63565bfb66d08055c78047693efe82b3f6549
Pentrick <bibenina09@mail.ru>
2025-09-21 10:27:11 +0300

    vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5001',
-    },
-  },
   esbuild: {
     jsx: 'automatic',
   },
